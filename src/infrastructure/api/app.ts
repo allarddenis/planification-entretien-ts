556001766683633c7d6abf9f8d619ef5b3f23afc
Denis <denis.allard.pro@gmail.com>
2024-10-03 09:55:44 +0200

    fix tests

--- src/infrastructure/api/app.ts
+++ src/infrastructure/api/app.ts
@@ -5,7 +5,8 @@ export const app: Application = express();
 const server: Server = new Server(app);
 const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
 
-app
+if (process.env.NODE_ENV !== "test") {
+  app
   .listen(PORT, "localhost", function () {
     console.log(`Server is running on port ${PORT}.`);
   })
@@ -16,3 +17,6 @@ app
       console.log(err);
     }
   });
+}
+
+
